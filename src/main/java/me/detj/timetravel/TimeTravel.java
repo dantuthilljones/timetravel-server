6c847cdbf4f7512c6644daa2435cb0628cfc2000
Dan <dan@detj.me>
2019-12-28 22:48:08 +0000

    Changes to run as spring boot application

--- src/main/java/me/detj/timetravel/TimeTravel.java
+++ src/main/java/me/detj/timetravel/TimeTravel.java
@@ -1,8 +1,11 @@
 package me.detj.timetravel;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-public class TimeTravel {
+@SpringBootApplication
+public class TimeTravel extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TimeTravel.class, args);
